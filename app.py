c762815bd6f391cd60fd890970da65965f6bb64c
NIKITA KHANDAL <78340078+NIKITA0509-I@users.noreply.github.com>
2022-07-28 09:18:25 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 02:01:44 2022
+
+@author: hp
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('linearregression.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model price for given square feet : {}'.format(prediction))
+
+
+app.run(debug=True)
